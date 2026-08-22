64ee33d1af5a7076535c06ce1f3f092166d1300d
shwetakulkarni1106-hub <shwetakulkarni1106@gmail.com>
2026-08-22 17:27:09 +0530

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,15 @@
+age=20
+print(age)
+print(type(age))
+
+name="shweta"
+print(name)
+print(type(name))
+
+cgpa=9.5
+print(cgpa)
+print(type(cgpa))
+
+is_completed=True  #also False
+print(is_completed)
+print(type(is_completed))
---
+++ typecasting.py
@@ -0,0 +1,13 @@
+a=10
+print(type(a))
+b=str(a)
+print(type(b))
+
+c=float(a)
+print(c)
+print(type(c))
+
+d="20"
+print(type(d))
+e=int(d)
+print(type(e))
