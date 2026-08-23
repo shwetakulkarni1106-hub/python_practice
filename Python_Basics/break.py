1d9101aa819ac5c0f1f456c79029aa42dbafedab
shwetakulkarni1106-hub <shwetakulkarni1106@gmail.com>
2026-08-23 17:33:11 +0530

    Add files via upload

---
+++ Python_Basics/break.py
@@ -0,0 +1,5 @@
+for i in range(0,21):
+    print(i)
+    if(i==10):
+        break 
+    
---
+++ Python_Basics/if_elif_else.py
@@ -0,0 +1,8 @@
+age=int(input("enter your age: "))
+if(age>18):
+    print("you can drive")
+elif(age==18):
+    print("you can learn now how to drive")
+else:
+    print("you cannot drive")
+print("program end")          
