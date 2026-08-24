4ff612dfa630207ca3047aea72ef8be143620817
shwetakulkarni1106-hub <shwetakulkarni1106@gmail.com>
2026-08-24 07:36:51 +0530

    Add files via upload

---
+++ Python_Basics/practice_3.2.py
@@ -0,0 +1,5 @@
+age=int(input("enter your age: "))
+if(age>=18):
+    print("you can vote")
+else:
+    print("you cannot vote.\n","you can vote after",18-age,"years")    
---
+++ Python_Basics/practice_3.4.py
@@ -0,0 +1,17 @@
+num=int(input("enter number inbetween(1 to 7)to print days: "))
+
+match num:
+    case 1: 
+        print("Sunday")
+    case 2: 
+        print("Monday")    
+    case 3: 
+        print("Tuesday")
+    case 4: 
+        print("Wednesday")
+    case 5: 
+        print("Thrusday")            
+    case 6: 
+        print("Friday")
+    case 7: 
+        print("Saturday")        
