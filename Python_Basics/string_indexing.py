334785708e5b23b8bb940b432cade92048528ceb
shwetakulkarni1106-hub <shwetakulkarni1106@gmail.com>
2026-08-25 07:25:37 +0530

    Add files via upload

---
+++ Python_Basics/string_indexing.py
@@ -0,0 +1,20 @@
+name="Shweta"
+
+#  s   h   w   e   t    a
+#  0   1   2   3   4    5
+#  -6  -5  -4  -3  -2  -1
+'''positive indexing
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])'''
+
+#negative indexing
+print(name[-1]) #(-1+6) it finds the positive index
+print(name[-2])
+print(name[-3])
+print(name[-4]) #(-4+len(name))   len(name)=6
+print(name[-5])
+print(name[-6])
