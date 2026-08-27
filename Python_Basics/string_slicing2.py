009babfdac96360d6d385a61ad3d966a5ee694e8
shwetakulkarni1106-hub <shwetakulkarni1106@gmail.com>
2026-08-27 22:17:32 +0530

    Add files via upload

---
+++ Python_Basics/string_slicing2.py
@@ -0,0 +1,4 @@
+name="Shweta123456789"
+print(name[1:8:1])     #no chane in string
+print(name[1:8:2])     #print characters with gap of (2-1=1) character
+print(name[1:12:3])    #print characters with gap of (3-1=2) character
