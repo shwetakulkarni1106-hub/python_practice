6a1aca6969e346d03ad430c162103ed6a63679fe
shwetakulkarni1106-hub <shwetakulkarni1106@gmail.com>
2026-08-29 17:16:29 +0530

    Add files via upload

---
+++ Python_Basics/practice_5.12.py
@@ -0,0 +1,9 @@
+def multiply(a,b):
+    """here multiplication of two number happens
+        c=a*b 
+       here c stores multiplication value of a*b    """
+    return a*b
+
+print(multiply(2,3))
+print(multiply(4,6))
+help(multiply)
